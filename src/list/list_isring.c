80e89d87109a4040e5e009b8f653e9a1d254d4f5
unknown <916421004@qq.com>
2020-09-22 18:29:20 +0800

    list is ring

---
+++ src/list/list_isring.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+#include <stdlib.h>
+typedef struct ListNode
+{
+    int val;
+    struct ListNode *next;
+}ListNode;
+int hasCycle(struct ListNode* head ) {
+    // write code here
+    struct ListNode* fast = head;
+    struct ListNode* low = head;
+    while(fast != NULL && fast->next != NULL) {
+        low = low->next;
+        fast = fast->next->next;
+        if (low == fast) {
+            return 1;
+        }
+    }
+    return 0;
+    
+}
+ListNode *create_list()
+{
+    static ListNode a,b,c,d,e;
+    ListNode *head = NULL;
+
+    int val = 0;
+    a.next = &b;
+    b.next = &c;
+    c.next = &d;
+    d.next = &e;
+    e.next = &a;
+
+    head = &a;
+
+    return &a;
+}
+int main()
+{
+    ListNode *head = create_list();
+    int ans = hasCycle(head);
+
+    printf("%d\n",ans);
+
+    
+    system("pause");
+    return 0;
+
+}
